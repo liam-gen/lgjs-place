a7908c7300d05a9096e1894948741dce42376e86
liamgen <liamgen.js@proton.me>
2023-10-11 16:03:22 +0200

    v1.0.5

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function createWindow() {
 
     /* AUTO UPDATER */
 
-    /*win.once('ready-to-show', () => {
+    win.once('ready-to-show', () => {
         autoUpdater.checkForUpdatesAndNotify();
     });
 
@@ -36,35 +36,14 @@ function createWindow() {
 
     autoUpdater.on('update-downloaded', () => {
         mainWindow.webContents.send('update_downloaded');
-    });*/
+    });
 
 }
 
-/*ipcMain.on('restart_app', () => {
+ipcMain.on('restart_app', () => {
     autoUpdater.quitAndInstall();
-  });*/
-
-  autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
-  })
-  autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.');
-  })
-  autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
-  })
-  autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater. ' + err);
-  })
-  autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
-  })
-  autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded');
   });
+
 
 app.whenReady().then(createWindow);
 
